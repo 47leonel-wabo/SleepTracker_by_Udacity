5ab950b79afc088efa4161d9965cf4c6e68ca9ce
leonel ka <waboleonel@gmail.com>
2020-05-04 13:14:14 +0100

    Adding entity annotated class

---
+++ app/src/main/java/com/aiwamob/sleeptracker/model/ASleep.kt
@@ -0,0 +1,10 @@
+package com.aiwamob.sleeptracker.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class ASleep(@PrimaryKey(autoGenerate = true) var sleepId: Long = 0L,
+             private val startTime: Long = System.currentTimeMillis(),
+             var endTime: Long = startTime,
+             var sleepQuality: Int = -1)
